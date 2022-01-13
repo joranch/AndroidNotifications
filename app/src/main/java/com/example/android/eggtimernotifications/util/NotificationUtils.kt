90a4afd9b497f029f230ca57af7a7bb0978314da
Jøran Haines <joran.haines@gmail.com>
2022-01-13 12:10:01 +0530

    add pending intent to notification

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -38,12 +38,11 @@ private val FLAGS = 0
  * @param context, activity context.
  */
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
-    // Create the content intent for the notification, which launches
-    // this activity
-    // TODO: Step 1.11 create intent
 
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
     // TODO: Step 1.12 create PendingIntent
-
+    val pendingIntent = PendingIntent.getActivity(
+        applicationContext, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
     // TODO: Step 2.0 add style
 
     // TODO: Step 2.2 add snooze action
@@ -52,14 +51,11 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         applicationContext,
         applicationContext.resources.getString(R.string.egg_notification_channel_id))
 
-    // TODO: Step 1.8 use the new 'breakfast' notification channel
-
     builder.setSmallIcon(R.drawable.egg_icon)
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
-
-
-    // TODO: Step 1.13 set content intent
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true)
 
         // TODO: Step 2.1 add style to builder
 
@@ -67,8 +63,5 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 
         // TODO: Step 2.5 set priority
 
-    // TODO: Step 1.4 call notify
     notify(NOTIFICATION_ID, builder.build())
 }
-
-// TODO: Step 1.14 Cancel all notifications
